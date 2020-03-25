5e57e271ebc8f7d135801004d77d98c093ecff18
Shaw Coney <rockconey@gmail.com>
2020-03-25 14:27:13 -0500

    first commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1453058849l/26156471.jpg';
+  image3 = 'https://i.pinimg.com/originals/71/42/48/7142480934ae21fbeaa75bc7486cad96.jpg';
 
   constructor() { }
 
